ab9ec43c4664a791c24b4ca8329a07bb64f3b452
n00bady <kaz00@vivaldi.net>
2022-11-01 23:20:54 +0200

    Added solution for part 2 of Day 2.

--- aoc-2016/Day_02/day02.go
+++ aoc-2016/Day_02/day02.go
@@ -23,8 +23,8 @@ func main()  {
   for _, lin := range input {
     for _, c := range lin {
       switch c {
-        case 'D': if y < 2 { y += 1 }
         case 'U': if y > 0 { y -= 1 }
+        case 'D': if y < 2 { y += 1 }
         case 'L': if x > 0 { x -= 1 }
         case 'R': if x < 2 { x += 1 }
       }
@@ -34,5 +34,35 @@ func main()  {
   fmt.Println("-=Part_1=-")
   fmt.Println(finalpos)
   fmt.Println("-=Part_2=-")
-  fmt.Println("to be contineued...")
+  part2()
+}
+
+
+func part2()  {
+  keypad := [][]int {{0, 0, 1, 0, 0}, {0, 2, 3, 4, 0}, {5, 6, 7, 8, 9}, {0, 0xA, 0xB, 0xC, 0}, {0, 0, 0xD, 0, 0}}
+  
+  file, _ := os.Open("input.txt")
+  scanner := bufio.NewScanner(file)
+  scanner.Split(bufio.ScanLines)
+  var input []string
+  for scanner.Scan() {
+    input = append(input, scanner.Text())
+  }
+  file.Close()
+  
+  x, y := 2, 0
+  var finalpos []int
+
+  for _, line := range input {
+    for _, ch := range line {
+      switch ch {
+      case 'U': if x > 0 { x -= 1; if keypad[x][y] == 0 { x += 1 } }
+      case 'D': if x < 4 { x += 1; if keypad[x][y] == 0 { x -= 1 } }
+      case 'L': if y > 0 { y -= 1; if keypad[x][y] == 0 { y += 1 } }
+      case 'R': if y < 4 { y += 1; if keypad[x][y] == 0 { y -= 1 } }
+      }
+    }
+    finalpos = append(finalpos, keypad[x][y])
+  }
+  fmt.Printf("%x\n", finalpos)
 }
