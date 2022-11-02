35c3a87dc395263cfb0bf2fee324c4122aaf3e8b
n00bady <kaz00@vivaldi.net>
2022-11-02 22:15:11 +0200

    Added solution for part 2 of Day 3 and some comments.

--- aoc-2016/Day_03/day03.go
+++ aoc-2016/Day_03/day03.go
@@ -20,13 +20,14 @@ func main()  {
   }
   file.Close()
   fmt.Println("-=Part_1=-")
-  // run part1 func
   part1(input)
   fmt.Println("-=Part_2=-")
-  // run part2 func
+  part2(input)
 }
 
-func checkifvalid(sides []int) bool {
+// check is a the 3 sides are a valid triangle 
+// it takes as input a slice of 3 ints and returns true or false
+func checkifvalidTriangle(sides []int) bool {
   if sides[0] + sides[1] > sides[2] && sides[1] + sides[2] > sides[0] && sides[2] + sides[0] > sides[1] {
     return true
   }
@@ -38,6 +39,11 @@ func part1(input [][]string)  {
   var t []int
   counter := 0
 
+  // Take the input line by line convert each string to an in 
+  // then add them to a slice of ints and send them to 
+  // checkifvalidTriangle to check if they are valid triangle
+  // if they are valid increase the counter by 1
+  // final result is the counter
   for _, slice := range input {
     for _, line := range slice {
       s := strings.Fields(line)
@@ -45,16 +51,61 @@ func part1(input [][]string)  {
         temp, _ := strconv.Atoi(n)
         t = append(t, temp)
       }
-      // if t[0] + t[1] > t[2] && t[1] + t[2] > t[0] && t[2] + t[0] > t[1]{
-      //   fmt.Println(t, " is correct!")
-      //   counter += 1
-      // }
-      if checkifvalid(t) {
+      if checkifvalidTriangle(t) {
         counter += 1
       }
       t = nil
     }
   }
+  fmt.Println(counter)
+}
 
+// Surely it must be a better and cleaner way to do this
+// but this is what I came up on my own at that time.
+func part2(input [][]string)  {
+  var triangles [1992][3]int
+  var t []int
+  var transposed [3][1992]int
+  counter := 0
+  i := 0
+  j := 0
+  k := 0
+
+  // Similar thing as in part1 but now I convert them to in 
+  // I put them in a slice of slices each row is a line and
+  // each column is one side of a triangle
+  for _, slice := range input {
+    for _, line := range slice {
+      s := strings.Fields(line)
+      for _, n := range s {
+        temp, _ := strconv.Atoi(n)
+        triangles[i][j] = temp
+        j += 1
+      }
+      j = 0
+      i += 1
+    }
+  }
+
+  // Then I converted the columns to rows and saved it as transposed
+  for i=0; i<3; i++ {
+    for j=0; j<1992; j++ {
+      transposed[i][j] = triangles[j][i]
+    }
+  }
+  // Then I iterated throught the transposed with the a step of 3
+  // and I checked those 3 values if they are valid triangles
+  // and increaed the counter if they are.
+  for i=0; i<3; i++ {
+    for j=0; j<1992; j+=3 {
+      for k=0; k<3; k++ {
+        t = append(t, transposed[i][j+k])
+      }
+      if checkifvalidTriangle(t) {
+        counter += 1
+      }
+      t = nil
+    }
+  }
   fmt.Println(counter)
 }
