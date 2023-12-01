05bbeebf6bc588392dcb184b7ad44fb6834bc959
n00bady <kaz00@vivaldi.net>
2023-12-01 20:24:04 +0200

    Improved part 2, got close to a solution but still no cigar.

--- aoc-2023/Day_01/GO/main.go
+++ aoc-2023/Day_01/GO/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"strconv"
+	"strings"
 	"unicode"
 
 	aoc "gitlab.com/n00bady/myaochelper"
@@ -13,19 +13,53 @@ func main() {
 	fmt.Println("-= AoC 2023 Day 01 Part 1")
 	part1()
 	fmt.Println("-= AoC 2023 Day 01 Part 2")
-    part2()
+	part2()
 }
 
 func part2() {
-    // I have no idea how to make it match eighthree as eight and three ???
-    m := regexp.MustCompile(`((f(ive|our)|s(even|ix)|t(hree|wo)|(ni|o)ne|eight))`)
-	f := aoc.Read_Text_Input("./test_input_2.txt")
+    // Although all my examples inputs seems to work 
+    // I can't get correct output in the actual input
+	f := aoc.Read_Text_Input("./input.txt")
 	lines := aoc.Parse_Lines(f)
+	sum := 0
+	numbers := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
 
-    for _, l := range lines {
-        res := m.FindAllString(l, 10)
-        fmt.Println(res)
-    }
+	for _, l := range lines {
+		var digits [100]int
+		var clean []int
+
+		for n := range numbers {
+			if strings.Index(l, n) > -1 {
+				digits[strings.Index(l, n)] = numbers[n]
+			}
+		}
+		for i, c := range l {
+			if unicode.IsDigit(c) {
+				digits[i] = int(c - '0')
+			}
+		}
+		for _, v := range digits {
+			if v != 0 {
+				clean = append(clean, v)
+			}
+		}
+		sum += clean[0]*10 + clean[len(clean)-1]
+
+		fmt.Println(clean)
+	}
+	f.Close()
+
+	fmt.Println("sum: ", sum)
 }
 
 func part1() {
@@ -46,6 +80,8 @@ func part1() {
 		}
 		sum += num
 	}
+
+	f.Close()
 
 	fmt.Println("sum: ", sum)
 }
